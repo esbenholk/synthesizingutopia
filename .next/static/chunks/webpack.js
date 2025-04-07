639fdb0ab9644f118b442f036c908677dd65ade0
esben holk nielsen <esbenholknielsen@gmail.com>
2025-04-07 15:21:37 +0200

    removed access to upload tool: interim

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "031a7249e25c6f82"; }
+/******/ 		__webpack_require__.h = function() { return "934bf26f4250bdde"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-"use client" 
-import { useEffect, useState } from 'react';
-import '../index.css'; // Import global CSS
-import { Upload } from '../components/Upload';
-
+"use client";
+import { useEffect, useState } from "react";
+import "../index.css"; // Import global CSS
+import { Upload } from "../components/Upload";
 
 export default function Home() {
   // const colors = ['#632bf3', '#f122c8', '#f16022', '#9ef344', '#44d3f3'];
@@ -18,11 +17,33 @@ export default function Home() {
     };
   }, []);
 
-
   return (
-    <main className="dont look under the hood">
-                  <img className="logo" style={{maxWidth: windowHeight-50}}src="https://res.cloudinary.com/dmwpm8iiw/image/upload/v1741868576/logo_efs6jc.gif"/>
-                  <Upload />
+    <main className="brew-container">
+      <img
+        className="logo"
+        style={{ maxWidth: windowHeight - 50 }}
+        src="https://res.cloudinary.com/dmwpm8iiw/image/upload/v1741868576/logo_efs6jc.gif"
+      />
+
+      <video
+        className={"couldron"}
+        src={
+          "https://res.cloudinary.com/dmwpm8iiw/video/upload/v1741863927/loopcouldron_svu0rw.mp4?q_auto:eco"
+        }
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+
+      <img
+        className={"overlay"}
+        src="https://res.cloudinary.com/dmwpm8iiw/image/upload/v1741865808/couldronoverlay_bg8osp.png"
+      />
+      <div className="desktopborder1"></div>
+      <div className="desktopborder2"></div>
+      {/* <Upload /> */}
+      <h1 className="notification">nyt værk på vej</h1>
     </main>
   );
 }
