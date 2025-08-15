c5481332bfbf33cacd78252b02824f96bc209137
esben holk nielsen <esbenholknielsen@gmail.com>
2025-08-15 10:02:33 +0200

    no content on auto api

--- src/app/api/autotagallimages/route.ts
+++ src/app/api/autotagallimages/route.ts
@@ -1,49 +1,49 @@
-import { NextResponse } from "next/server";
-import { v2 as cloudinary } from "cloudinary";
+// import { NextResponse } from "next/server";
+// import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
-type Image = {
-  id: string;
-  url: string;
-  title: string;
-  tags: string[];
-  date: string;
-};
+// type Image = {
+//   id: string;
+//   url: string;
+//   title: string;
+//   tags: string[];
+//   date: string;
+// };
 
-export async function GET(request: Request) {
-  const skipNumber = 88;
-  const limitNumber = 1000;
-  const recentImagesResponse = await fetch(
-    `${process.env.BASE_URL}/api/cloudinary/recent?limit=${encodeURIComponent(
-      limitNumber
-    )}&skip=${encodeURIComponent(skipNumber)}`
-  );
-  const data = await recentImagesResponse.json();
+// export async function GET(request: Request) {
+//   const skipNumber = 88;
+//   const limitNumber = 1000;
+//   const recentImagesResponse = await fetch(
+//     `${process.env.BASE_URL}/api/cloudinary/recent?limit=${encodeURIComponent(
+//       limitNumber
+//     )}&skip=${encodeURIComponent(skipNumber)}`
+//   );
+//   const data = await recentImagesResponse.json();
 
-  for (let index = 0; index < data.length; index++) {
-    const element = data[index];
+//   for (let index = 0; index < data.length; index++) {
+//     const element = data[index];
 
-    console.log(element);
+//     console.log(element);
 
-    const response = await fetch(
-      `${process.env.BASE_URL}/api/cloudinary/upload`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          imageUrl: element.url,
-          title: element.title,
-        }),
-      }
-    );
-  }
+//     const response = await fetch(
+//       `${process.env.BASE_URL}/api/cloudinary/upload`,
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           imageUrl: element.url,
+//           title: element.title,
+//         }),
+//       }
+//     );
+//   }
 
-  return NextResponse.json(data);
-}
+//   return NextResponse.json(data);
+// }
